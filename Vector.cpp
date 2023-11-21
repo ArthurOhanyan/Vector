3b6202fe538c1f6cdf6bd145da5db865065cda5f
Arthur <arthur@Linux.myguest.virtualbox.org>
2023-11-21 20:08:04 +0400

    added assign and some other funcions

--- Vector.cpp
+++ Vector.cpp
@@ -12,7 +12,7 @@ class MyVector
 		T* tmp = new T [v_capacity];
 		for (int i = 0; i < v_size; ++i){
 			
-			tmp [i] == v_ptr[i];
+			tmp [i] = v_ptr[i];
 		}	
 		delete [] v_ptr;
 		v_ptr = tmp;
@@ -38,19 +38,6 @@ class MyVector
 		v_capacity = 0;
 	}
 
-	/*void reallocator (int x = 0){
-			
-		x == 0 ? v_capacity *= 2 : v_capacity = x;
-		T* tmp = new T [v_capacity];
-		for (int i = 0; i < v_size; ++i){
-			
-			tmp [i] == v_ptr[i];
-		}	
-		delete [] v_ptr;
-		v_ptr = tmp;
-		tmp = nullptr;
-	}*/
-
 	int size ();
 	size_t max_size ();
 	void resize (int);
@@ -58,7 +45,11 @@ class MyVector
 	bool empty ();
 	void reserv (int);
 	void shrink_to_fit();
-
+	T& at (int);
+	T& front ();
+	T& back ();
+	T* data ();
+	void assign (int,const T&);
 	
 };
 int main ()
@@ -75,6 +66,21 @@ int main ()
         std::cout << " after reserv capacity () :: " << my_vec.capacity () << std::endl;	
 	my_vec.shrink_to_fit();
 	std::cout << " after shrink to fit capacity is :: " << my_vec.capacity() << std::endl;
+	std::cout << " at () function called :: " << my_vec.at(2) << std::endl;
+	std::cout << " front () function called :: " << my_vec.front() << std::endl;
+	std::cout << " back () function called :: " << my_vec.back() << std::endl;
+	for (int i = 0; i < my_vec.size(); ++i){
+		
+		my_vec.at(i) = i;
+		std::cout << " " << *(my_vec.data() + i) ;
+	}
+	std::cout << std::endl;
+	my_vec.assign (10,9);
+	for (int i = 0; i < my_vec.size(); ++i){
+		
+		std::cout << " " << my_vec.at(i);
+	}
+	std::cout << std::endl;
 
 }
 
@@ -102,18 +108,6 @@ void MyVector<T>::resize (int new_size)
 	if (new_size > v_capacity){
 		
 		MyVector<T>::reallocator(new_size);
-		/*
-		 * v_capacity = new_size;
-		T* tmp = new T [v_capacity];
-		for (int i = 0; i < v_size; ++i){
-			
-			tmp[i] = v_ptr[i];
-			
-		}
-		delete [] v_ptr;
-		v_ptr = tmp;
-		tmp = nullptr; 
-		*/
 
 	}
        
@@ -125,7 +119,8 @@ void MyVector<T>::resize (int new_size)
 		
 		for (int i = v_size; i < new_size; ++i){
 			
-			v_ptr[v_size++] = 0;
+			v_ptr[i] = 0;
+			++v_size;
 		}
 	}
 }
@@ -158,4 +153,72 @@ void MyVector<T>::shrink_to_fit()
 		
 		MyVector<T>::reallocator(v_size);
 	}
+}
+
+template <typename T>
+T& MyVector<T>::at (int n)
+{
+	if (!v_ptr){
+		
+		std::cout << " at() function :: nullptr :: error " << std::endl;
+		//exit(0);
+	}
+
+	if (n >= v_size || n < 0){
+		
+		std::cout << " at() function :: out_of_range " << std::endl;
+		exit(0);
+	} else {
+	
+		return v_ptr[n];
+	}
+}
+
+template <typename T>
+T& MyVector<T>::front ()
+{
+	if (!v_ptr){
+		
+		std::cout << " front () function :: nullptr :: error " << std::endl;
+		exit(0);
+	}
+
+	return v_ptr[0];
+}
+
+template <typename T>
+T& MyVector<T>::back ()
+{
+	if (!v_ptr){
+		
+		std::cout << " back () function :: nullptr :: error " << std::endl;
+		exit (0);
+	}
+
+	return v_ptr[v_size - 1];
+}
+
+template <typename T>
+T* MyVector<T>::data ()
+{
+	return v_ptr;
+}
+
+template <typename T>
+void MyVector<T>::assign (int count,const T& value)
+{
+	if (count > v_capacity){
+		
+		v_capacity = count + 5;
+	}
+
+	T* tmp = new T [v_capacity];
+	for (int i = 0; i < count; ++i){
+		
+		tmp [i] = value;
+	}
+	v_size = count;
+	delete [] v_ptr;
+	v_ptr = tmp;
+	tmp = nullptr;
 }
